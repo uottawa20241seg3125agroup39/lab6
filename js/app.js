5bb61e99a0b7025db28bc921e2795cb699b31320
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-03-03 13:27:43 -0500

    switch to nodejs framework

--- js/app.js
+++ js/app.js
@@ -1,20 +1,11 @@
-window.onload = function() {
-  console.log('Load completed');
-}
-function submitForm(){
-  // check the empty fields
-  var q11=$('input[name="firstname"]').val()==='';
-  var q12=$('input[name="lastname"]').val()==='';
-  var q2=!$('input[name="years"]:checked').val();
-  var q3=!$('input[name="q3-choice"]:checked').val();
-  var q4=!$('#dropdown-question').val();
-  var q5=!$('input[name="q5-choice"]:checked').val();
-  var q6=!$('#rate').val();
-  var q7=!$('input[name="q7-choice"]:checked').val();
-  var q8=!$('input[name="q8-choice"]:checked').val();
-  if (q11 || q12 || q2 || q3 || q4 || q5 || q6 || q7 || q8){
-    alert("Please answer all questions");
-    return false;
-  }
-  window.location.href = "submitted.html";
-}
+let express = require('express');
+const {join} = require("path");
+
+
+let app= express();
+app.get('/', function(req, res) {
+  res.sendFile(join(__dirname, 'index.html'));
+})
+
+app.listen(800);
+console.log('Server is running on port 800')
--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    app: './js/app.js',
+    app: './js/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: './js/app.js',
+    filename: './js/index.js',
   },
 };
