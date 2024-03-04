ad43f2783523060546dc5a07915e147ba51aac46
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-03-04 02:18:58 -0500

    result display

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 let express = require('express');
 const {join} = require("path");
 const processor = require('./result_processor');
+const db=require('./memdb');
 
 
 let app= express();
 app.use(express.static(join(__dirname, '../')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, '../view'));
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: join(__dirname, '../view')});
 })
@@ -21,6 +24,11 @@ app.post('/submit', function(req, res) {
   processor(req,res);
   // console.log(req.body.length);
 })
+
+app.get('/result', function(req, res) {
+  console.log('Result requested');
+  res.render('result', {data: db.get()});
+});
 
 app.listen(800);
 console.log('Server is running on port 800')
--- js/memdb.js
+++ js/memdb.js
@@ -10,13 +10,7 @@ function increase(question,rate){
   mem[question][rate]++;
   console.log(`new data registered for ${question} . ${rate} : ${mem[question][rate]}`);
 }
-function get(question,rate){
-  if (!question in mem) {
-    return 0;
-  }
-  if (!rate in mem[question]) {
-    return 0;
-  }
-  return mem[question][rate];
+function get(){
+  return structuredClone(mem);
 }
 module.exports={increase,get};
